f623211634a6d2df0d369602e28b50aa7855a09b
Yakov Popov <suburbanms@gmail.com>
2022-11-13 23:53:53 +0700

    Добавьте файлы проекта.

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,156 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+// начальные данные
+List<Person> users = new List<Person>
+{
+    new() { Id = Guid.NewGuid().ToString(), Name = "Tom", Age = 37 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Bob", Age = 41 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Sam", Age = 24 }
+};
+
+        var builder = WebApplication.CreateBuilder();
+        var app = builder.Build();
+
+        app.UseDefaultFiles();
+app.UseStaticFiles();
+ 
+app.MapGet("/api/users", ()=> users);
+ 
+app.MapGet("/api/users/{id}", (string id) =>
+{
+    // получаем пользователя по id
+    Person? user = users.FirstOrDefault(u => u.Id == id);
+    // если не найден, отправляем статусный код и сообщение об ошибке
+    if (user == null)  return Results.NotFound(new { message = "Пользователь не найден" });
+ 
+    // если пользователь найден, отправляем его
+    return Results.Json(user);
+});
+
+app.MapDelete("/api/users/{id}", (string id) =>
+{
+    // получаем пользователя по id
+    Person? user = users.FirstOrDefault(u => u.Id == id);
+
+    // если не найден, отправляем статусный код и сообщение об ошибке
+    if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
+
+    // если пользователь найден, удаляем его
+    users.Remove(user);
+    return Results.Json(user);
+});
+
+app.MapPost("/api/users", (Person user) => {
+
+    // устанавливаем id для нового пользователя
+    user.Id = Guid.NewGuid().ToString();
+    // добавляем пользователя в список
+    users.Add(user);
+    return user;
+});
+
+app.MapPut("/api/users", (Person userData) => {
+
+    // получаем пользователя по id
+    var user = users.FirstOrDefault(u => u.Id == userData.Id);
+    // если не найден, отправляем статусный код и сообщение об ошибке
+    if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
+    // если пользователь найден, изменяем его данные и отправляем обратно клиенту
+
+    user.Age = userData.Age;
+    user.Name = userData.Name;
+    return Results.Json(user);
+});
+
+app.Run();
+
+public class Person
+{
+    public string Id { get; set; } = "";
+    public string Name { get; set; } = "";
+    public int Age { get; set; }
+}
+    }
+}
